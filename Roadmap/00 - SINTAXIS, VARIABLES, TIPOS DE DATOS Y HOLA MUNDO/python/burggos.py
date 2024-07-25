d8d0a3911af205a9ae93df0e600da597d2275fd6
burggos <gabrielbucarra17@gmail.com>
2024-07-24 19:04:49 -0500

    00-Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/burggos.py
@@ -0,0 +1,24 @@
+#https://www.python.org/ ... Esta es la pagina oficial del lenguaje.
+
+"""
+Esta es otra manera de comentar en Python,
+sirve para comnetar en varias lineas.
+"""
+
+'''
+Esta es otra manera de comentar en varias lineas,
+entre gustos no hay disgustos.
+'''
+
+mi_varible = "¡Hola Mundo!"  #Esto es una variable
+MI_CONSTANTE = "¡Chao Mundo!" #En Python no exixten las constantes, pero en mayusculas se sobrentiende
+
+cadenas = "hello" #variable de tipo cadena de texto
+enteros = 10 #Variable con tipo de datos entero
+
+flotantes = 2.3 # Variable con tipo de dato flotante
+
+booleano = True #Variable con tipo de dato booleano(Inicial mayuscula)
+booleano2 = False #El otro tipo de booleano(Inicial mayuscula) 
+
+print("Hola,Python")
